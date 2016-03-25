5233eb2a903f6c2b6e4a1f6dc08e1bd0abdc9c51
liuwons <liuwons@163.com>
2016-03-25 15:46:58 +0800

    deletes

--- editor/config.cpp
+++ editor/config.cpp
@@ -9,18 +9,3 @@ QString CONFIG_STRING_EVERNOTE_TOKEN = "evernote_token";
 QString CONFIG_STRING_EVERNOTE_ACCOUNT_TYPE = "evernote_account_type";
 QString CONFIG_STRING_MARKDOWN_THEME = "markdown_theme";
 QString WORKBENCH_CONFIG_FILE_NAME = ".emworkbench";
-
-QSize screenSize;
-QString theme = "github";
-QString appDir;
-QString cssSubDir = "css";
-
-QtAwesome* awesome = 0;
-
-QSettings* settings = 0;
-
-QString* workbenchPath = 0;
-QString* evernoteToken = 0;
-QString* evernoteAccountType = 0;
-
-QString* markdownTheme = 0;
--- editor/config.h
+++ editor/config.h
@@ -5,14 +5,11 @@
 #include <QString>
 #include <QSettings>
 
-
 extern QString CONFIG_STRING_WORKBENCH_PATH;
 extern QString CONFIG_STRING_EVERNOTE_TOKEN;
 extern QString CONFIG_STRING_EVERNOTE_ACCOUNT_TYPE;
 extern QString CONFIG_STRING_MARKDOWN_THEME;
 extern QString WORKBENCH_CONFIG_FILE_NAME;
-
-
 
 #endif // CONFIG_H
 
